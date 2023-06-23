839809beba1ed5ab87b3860c9f3c4821e3174ae8
Melany Espindola <mj.espindola98@gmail.com>
2023-06-23 02:04:48 -0300

    Agrega archivo js y selectores para formulario

---
+++ script.js
@@ -0,0 +1,7 @@
+const formContact = document.querySelector("#form-contact");
+const formName = document.querySelector("#form-name");
+const formLastName = document.querySelector("#form-lastname");
+const formCompany = document.querySelector("#form-company");
+const formEmail = document.querySelector("#form-email");
+const formSalary = document.querySelector("#form-salary");
+const formSubmit = document.querySelector("#form-submit");
