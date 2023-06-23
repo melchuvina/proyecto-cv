1e75c18ced3734e841f60cb0363d7cae39adec13
Melany Espindola <mj.espindola98@gmail.com>
2023-06-23 05:43:13 -0300

    Agrega lógica y validación de formulario

--- script.js
+++ script.js
@@ -1,7 +1,90 @@
-const formContact = document.querySelector("#form-contact");
-const formName = document.querySelector("#form-name");
-const formLastName = document.querySelector("#form-lastname");
-const formCompany = document.querySelector("#form-company");
-const formEmail = document.querySelector("#form-email");
-const formSalary = document.querySelector("#form-salary");
-const formSubmit = document.querySelector("#form-submit");
+const formContact = document.getElementById("form-contact");
+const formName = document.getElementById("form-name");
+const formLastName = document.getElementById("form-lastname");
+const formCompany = document.getElementById("form-company");
+const formEmail = document.getElementById("form-email");
+const formSalary = document.getElementById("form-salary");
+const formComment = document.getElementById("form-comment");
+const formSubmit = document.getElementById("form-submit");
+
+const errorClass = 'form-error';
+
+formSubmit.addEventListener('click', () => {
+    const allFieldsAreValid = validateFields();
+    if (allFieldsAreValid) {
+        //create comment container and append
+        //clear inputs
+    }
+})
+
+
+formName.addEventListener('focus', () => {
+    cleanError(formName);
+})
+
+formLastName.addEventListener('focus', () => {
+    cleanError(formLastName);
+})
+
+formCompany.addEventListener('focus', () => {
+    cleanError(formCompany);
+})
+
+formEmail.addEventListener('focus', () => {
+    cleanError(formEmail);
+})
+
+formSalary.addEventListener('focus', () => {
+    cleanError(formSalary);
+})
+
+formComment.addEventListener('focus', () => {
+    cleanError(formComment);
+})
+
+function cleanError(selector) {
+    selector.classList.remove(errorClass);
+}
+
+function validateFields() {
+
+    let allFieldsAreValid = true;
+    let errorMessage = 'Se han producido los siguientes errores en el formulario:'
+
+    if (!formName.value) {
+        allFieldsAreValid = false;
+        errorMessage += '\n    El campo Nombre está vacío'
+        formName.classList.add(errorClass);
+    }
+    if (!formLastName.value) {
+        allFieldsAreValid = false;
+        errorMessage += '\n    El campo Apellido está vacío'
+        formLastName.classList.add(errorClass);
+    }
+    if (!formCompany.value) {
+        allFieldsAreValid = false;
+        errorMessage += '\n    El campo Empresa está vacío'
+        formCompany.classList.add(errorClass);
+    }
+    if (!formEmail.value) {
+        allFieldsAreValid = false;
+        errorMessage += '\n    El campo Email está vacío'
+        formEmail.classList.add(errorClass);
+    }
+    if (formSalary.value === '0') {
+        allFieldsAreValid = false;
+        errorMessage += '\n    El campo Salario Mínimo es nulo'
+        formSalary.classList.add(errorClass);
+    }
+    if (!formComment.value) {
+        allFieldsAreValid = false;
+        errorMessage += '\n    El campo Mensaje está vacío'
+        formComment.classList.add(errorClass);
+    }
+
+    if (!allFieldsAreValid) {
+        alert(errorMessage);
+    }
+
+    return allFieldsAreValid
+}
